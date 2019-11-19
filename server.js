4d5192fdb3d3a17cb955990a5da4647c8f76c9a8
ramjibs <ramji.vishnu.26@gmail.com>
2019-11-19 22:33:58 +0530

    fix try 2 for module not found error

--- server.js
+++ server.js
@@ -1,32 +1,45 @@
-const keys = require('./config/Keys.js')
-const express = require('express')
+import { PORT } from './config/Keys';
+import express from 'express';
 const app = express();
-const bodyparser = require('body-parser')
-const helmet = require('helmet')
+import { urlencoded, json } from 'body-parser';
+import helmet from 'helmet';
 const debug = require('debug')('app:server')
-const cors = require('cors')
-const RegisterSchoolRouter = require('./routes/registerSchool')
-const UserRouter = require('./routes/user')
-const CheckTokenRouter = require('./routes/checktoken')
-const passport = require('passport')
+import cors from 'cors';
+import RegisterSchoolRouter from './routes/registerSchool';
+import UserRouter from './routes/user';
+import CheckTokenRouter from './routes/checktoken';
+import passport, { initialize } from 'passport';
 //make connection to the DB
-require('./config/dbconfig/db.connection')
+import './config/dbconfig/db.connection';
 
-app.use(passport.initialize())
+app.use(initialize())
 require('./config/passport')(passport);
 app.use(helmet())
-app.use(bodyparser.urlencoded({ extended: false}))
-app.use(bodyparser.json())
+app.use(urlencoded({ extended: false}))
+app.use(json())
 app.use(cors())
 app.use('/api/checktoken',CheckTokenRouter)
 app.use('/api/registration', RegisterSchoolRouter)
 app.use('/api/users',UserRouter)
 
 
-app.listen(keys.PORT, () =>{
-    debug(`Server started in Port ${keys.PORT}`)
+app.listen(PORT, () =>{
+    debug(`Server started in Port ${PORT}`)
 })
 
+// const keys = require('./config/Keys')
+// const express = require('express')
+// const app = express();
+// const bodyparser = require('body-parser')
+// const helmet = require('helmet')
+// const debug = require('debug')('app:server')
+// const cors = require('cors')
+// const RegisterSchoolRouter = require('./routes/registerSchool')
+// const UserRouter = require('./routes/user')
+// const CheckTokenRouter = require('./routes/checktoken')
+// const passport = require('passport')
+// //make connection to the DB
+// require('./config/dbconfig/db.connection')
 
 
 
